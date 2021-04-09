ee3c6bcef99eae1cb51d3ae03f3a458b38326a12
Vadim <vadim@vectorized.io>
2021-04-09 17:01:40 +0300

    Create template function for time bench

--- main.cpp
+++ main.cpp
@@ -5,8 +5,7 @@
 
 #include "wasmer-instance-db.h"
 #include "v8-instance-db.h"
-
-constexpr int64_t IT_COUNT = 7000000;
+#include "jemalloc/jemalloc.h"
 
 struct test_sum_t {
     explicit test_sum_t(int a_, int b_)
@@ -19,11 +18,14 @@ struct test_sum_t {
     int ans;
 };
 
-void simple_wasmer_bench() {
-    wasmer_instance_db inst;
-    inst.add_new_script("../examples/new-delete.wasm", "sum");
+constexpr int64_t IT_COUNT = 700000;
 
-    auto raw_ptr = inst.alloc_memory_in_wasm_script("sum", sizeof(test_sum_t));
+template<typename engine_type>
+double time_bench(const std::string& script_path) {
+    engine_type engine;
+    engine.add_new_script(script_path, script_path);
+
+    auto raw_ptr = engine.alloc_memory_in_wasm_script(script_path, sizeof(test_sum_t));
     assert(raw_ptr != nullptr);
     auto obj_ptr = reinterpret_cast<test_sum_t*>(raw_ptr);
 
@@ -33,7 +35,7 @@ void simple_wasmer_bench() {
         new (raw_ptr) test_sum_t(i, i + 1);
 
         auto start = std::chrono::high_resolution_clock::now();
-        inst.run_script("sum", raw_ptr);
+        engine.run_script(script_path, raw_ptr);
         auto end = std::chrono::high_resolution_clock::now();
 
         std::chrono::duration<double> time = end - start;
@@ -42,35 +44,8 @@ void simple_wasmer_bench() {
         obj_ptr->~test_sum_t();
 
     }
-
-    inst.delete_memory_in_wasm_script("sum", raw_ptr);
-    std::cout << "Wasmer bench: " << min_time << std::endl;
-}
-
-void simple_v8_bench() {
-    v8_instance_db v8;
-    v8.add_new_script("/home/vadim/wasm-engines/examples/simple.js", "simple");
-
-    auto raw_ptr = v8.alloc_memory_in_wasm_script("simple", sizeof(test_sum_t));
-    assert(raw_ptr != nullptr);
-    auto obj_ptr = reinterpret_cast<test_sum_t*>(raw_ptr);
-    double min_time = std::numeric_limits<double>::max();
-
-    for (int i = 0; i < IT_COUNT; ++i) {
-        new (raw_ptr) test_sum_t(i, i + 1);
-
-        auto start = std::chrono::high_resolution_clock::now();
-        v8.run_script("simple", raw_ptr);
-        auto end = std::chrono::high_resolution_clock::now();
-
-        std::chrono::duration<double> time = end - start;
-        min_time = std::min(min_time, time.count());
-
-        obj_ptr->~test_sum_t();
-    }
-
-    v8.delete_memory_in_wasm_script("simple", raw_ptr);
-    std::cout << "V8 bench: " << min_time << std::endl;
+    engine.delete_memory_in_wasm_script(script_path, raw_ptr);
+    return min_time;
 }
 
 void simple_bench_without_wasm() {
@@ -92,9 +67,14 @@ void simple_bench_without_wasm() {
     std::cout << "Simple bench: " << min_time << std::endl;
 }
 
+void time_comparison() {
+    simple_bench_without_wasm();
+    std::cout << "Wasmer bench: " << time_bench<wasmer_instance_db>("../examples/new-delete.wasm") << std::endl;
+    std::cout << "V8 bench: " << time_bench<v8_instance_db>("/home/vadim/wasm-engines/examples/simple.js") << std::endl;
+
+}
+
 int main() {
-    //simple_bench_without_wasm();
-    //simple_wasmer_bench();
-    simple_v8_bench();
+    time_comparison();
     return 0;
 }
