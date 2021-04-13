398c9fddfdb1df8eaf8c06ba568171590ccac909
Vadim <vadim@vectorized.io>
2021-04-13 12:25:43 +0300

    Fix js example

--- examples/sum_array.js
+++ examples/sum_array.js
@@ -1,6 +1,6 @@
 function user_script(obj) {
     let array = new Int32Array(obj);
-    for (i = 0; i < array[0]; ++i) {
+    for (let i = 0; i < array[0]; i++) {
         array[1] += array[i + 2];
     }
 }
