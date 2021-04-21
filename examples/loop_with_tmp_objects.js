efcbf95bd6d64d68c9af69b9c32f3b9f56a9e808
Vadim <vadim@vectorized.io>
2021-04-21 12:21:29 +0300

    Use while(true) for js script

--- examples/loop_with_tmp_objects.js
+++ examples/loop_with_tmp_objects.js
@@ -1,8 +1,9 @@
 function user_script(obj) {
-    let it_count = new Int32Array(obj);
     let array_map = new Map();
-    for (let i = 0; i < it_count; ++i) {
+    let i = 0;
+    while (true) {
         let tmp_array = new ArrayBuffer(100);
         array_map[i] = tmp_array;
+        i += 1;
     }
 }
